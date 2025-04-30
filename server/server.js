62503566055e0526df98c6e557373e7b1d517079
Samir Shuman <devmansam@protonmail.com>
2025-04-29 20:34:34 -0700

    .

--- server/server.js
+++ server/server.js
@@ -56,7 +56,7 @@ app.options("*", cors());
 // Ensure proper handling of preflight requests
 app.use((req, res, next) => {
   if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
+    res.header('Access-Control-Allow-Origin', req.headers.origin || '*');
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', 'true');
